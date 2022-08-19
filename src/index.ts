32f7ef7e3851ddf68effe2069670ea0a9b311016
고동현 <dhjk35@naver.com>
2022-08-19 14:41:33 +0900

    Chore(App): 클래스명 소문자로 변경 및 가져오기 설정 변경

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { createRoot } from '~/core';
-import App from './App';
+import App from './app';
 
 import '~/styles/global.scss';
 
-const $target: HTMLDivElement = document.querySelector('.App')!;
+const $target: HTMLDivElement = document.querySelector('.app')!;
 
 createRoot(App, $target);
