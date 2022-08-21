1d6495f6281963b6faaa952c9353ad7e92160b58
고동현 <dhjk35@naver.com>
2022-08-21 17:23:16 +0900

    Design(Button): 버튼 컴포넌트 기본 스타일 지정

--- src/components/button/Button.ts
+++ src/components/button/Button.ts
@@ -2,6 +2,8 @@ import { useEvent } from '~/core';
 
 import { removeWhiteSpaceOfString } from '~/utils';
 
+import styles from './Button.module.scss';
+
 interface ButtonProps {
   children: string;
   onClick: (e: MouseEvent) => void;
@@ -11,9 +13,11 @@ const Button = ({ children, onClick }: ButtonProps) => {
   useEvent(removeWhiteSpaceOfString(children), 'click', onClick);
 
   return `
-      <button data-event=${removeWhiteSpaceOfString(
-        children,
-      )}>${children}</button>
+      <button 
+        class=${styles.button}
+        data-event="${removeWhiteSpaceOfString(children)}">
+        ${children}
+      </button>
     `;
 };
 
