d8a5eff6ef93501f596d7a0883cfb006f483e22e
고동현 <dhjk35@naver.com>
2022-08-22 11:56:42 +0900

    Chore(ProgressBar): 내보내기 설정 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './button';
 export { default as Image } from './image';
 export { default as Section } from './section';
+export { default as ProgressBar } from './progress-bar';
