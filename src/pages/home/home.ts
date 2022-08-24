ffe401b51e2f6ed150a60728830b569d91c39e4e
고동현 <dhjk35@naver.com>
2022-08-24 14:28:08 +0900

    Feat(Home): ServiceSection 적용

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,14 @@
+import { Section } from '~/components';
+
 import MainSection from './main-section';
+import ServiceSection from './service-section';
 
 import styles from './Home.module.scss';
 
 const Home = () => {
   return `
     ${MainSection()}
+    ${Section({ children: ServiceSection() })}
   `;
 };
 
