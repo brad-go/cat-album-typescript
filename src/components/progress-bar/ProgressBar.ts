7bbb73c926f40dbe6960b91d745bd41f85f31f92
고동현 <dhjk35@naver.com>
2022-08-24 14:06:35 +0900

    Fix(MainSection): 렌더링 방식 변경으로 인한 스타일 적용 오류 해결

--- src/components/progress-bar/ProgressBar.ts
+++ src/components/progress-bar/ProgressBar.ts
@@ -1,19 +1,36 @@
+import { useEffect, useState } from '~/core';
 import styles from './ProgressBar.module.scss';
 
 interface ProgressBarProps {
   width: number;
   height: number;
   color: string;
-  isActive: boolean;
+  currentPage: number;
 }
 
-const ProgressBar = ({ width, height, color, isActive }: ProgressBarProps) => {
+const ProgressBar = ({
+  width,
+  height,
+  color,
+  currentPage,
+}: ProgressBarProps) => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsActive(false);
+  }, [currentPage]);
+
+  useEffect(() => {
+    setIsActive(true);
+  }, [isActive]);
+
   return `
     <div class=${styles.container} 
-      style="width: ${width}px; height: ${height}px">
-      <div class="${
-        isActive ? `${styles.active} ${styles.progress}` : styles.progress
-      }" style="background-color: ${color}"></div>
+      style="width: ${width}px; height: ${height}px"
+    >
+      <div class="
+        ${isActive ? `${styles.progress} ${styles.active}` : styles.progress}
+      " style="background-color: ${color}"></div>
     </div>
   `;
 };
