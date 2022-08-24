3dead44855d5dd2e09e4e9a56e67a5014d6cc2a9
고동현 <dhjk35@naver.com>
2022-08-24 13:56:01 +0900

    Refactor(Core): render 함수 리팩토링
    - render 함수에 diff 알고리즘 적용
    - 기존 문제: 상태가 변경되면 전체가 리렌더링 됨
    - 변경 이후: 기존 DOM과 변경 사항을 비교해 변경된 부분만을 리렌더링

--- src/core/render.ts
+++ src/core/render.ts
@@ -8,6 +8,80 @@ export const render = debounceFrame(() => {
 
   if (!element || !container) return;
 
-  container.innerHTML = element();
+  diffELements(element(), container);
+
   stateManager.currentIndex = 0;
 });
+
+const diffELements = (element: string, container: Element) => {
+  const parser = new DOMParser();
+  const newElements: Element[] = Array.from(
+    parser.parseFromString(element, 'text/html').body.children,
+  );
+
+  const newContainer: Node = container.cloneNode();
+  newElements.forEach((element) => newContainer.appendChild(element));
+
+  updateElement(document.body, container, newContainer as Element);
+};
+
+const updateElement = (
+  parent: Element,
+  oldNode: Element,
+  newNode: Element,
+  index = 0,
+) => {
+  if (oldNode && !newNode) {
+    return (oldNode as Element).remove();
+  }
+
+  if (!oldNode && newNode) {
+    return parent.appendChild(newNode);
+  }
+
+  // 둘 다 text타입일 경우
+  if (oldNode instanceof Text && newNode instanceof Text) {
+    if (oldNode.nodeValue === newNode.nodeValue) return;
+
+    oldNode.nodeValue = newNode.nodeValue;
+    return;
+  }
+
+  // 둘이 서로 태그 명이 다른 경우
+  if (oldNode.nodeName !== newNode.nodeName) {
+    oldNode.remove();
+    parent.appendChild(newNode);
+    return;
+  }
+
+  updateAttributes(oldNode, newNode);
+
+  const oldChildren = [...oldNode.childNodes];
+  const newChildren = [...newNode.childNodes];
+  const maxLength = Math.max(oldChildren.length, newChildren.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    updateElement(
+      oldNode,
+      oldChildren[i] as Element,
+      newChildren[i] as Element,
+    );
+  }
+};
+
+const updateAttributes = (oldNode: Element, newNode: Element) => {
+  const oldProps = [...oldNode.attributes];
+  const newProps = [...newNode.attributes];
+
+  // 달라지거나 추가된 props 반영
+  for (const { name, value } of newProps) {
+    if (value === oldNode.getAttribute(name)) continue;
+    oldNode.setAttribute(name, value);
+  }
+
+  // 없어진 props를 attributes에서 제거
+  for (const { name } of oldProps) {
+    if (newNode.getAttribute(name) !== undefined) continue;
+    oldNode.removeAttribute(name);
+  }
+};
