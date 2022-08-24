d96ebead504abef253a9535340601a83797a4cc9
고동현 <dhjk35@naver.com>
2022-08-24 14:23:14 +0900

    Feat(ServiceSection): ServiceSection 구현
    - 전역변수 timer를 설정하고, 이동이 일어날 때마다 clearInterval을
    사용해서 기존 react의 useEffect clenaUp 함수와 비슷한 효과를 냄.
    - moveSlide에서 index를 Number로 변경해주지 않으면 이동은 되지만, 오류가 남
    
    Ref. currentTarget을 사용하면 이벤트리스너가 등록된 대상을 반환하므로 클릭 이벤트에서
    다른 것이 클릭되지 않게 하기위해 내부요소에 pointer-events: none을 설정할 필요가 없음

---
+++ src/pages/home/service-section/ServiceSection.ts
@@ -0,0 +1,75 @@
+import type { Cat } from '~/types';
+
+import { useState, useEffect } from '~/core';
+import { getImageOfCats } from '~/api';
+import { PHRASES } from '~/constatns';
+
+import Slider from './slider';
+import ProgressBar from './progress-bar';
+
+import styles from './ServiceSection.module.scss';
+
+let timer: string | number | NodeJS.Timeout | undefined;
+
+const ServiceSection = () => {
+  const [cats, setCats] = useState<Cat[]>([]);
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const fetchCatImages = async () => {
+    try {
+      const response = await getImageOfCats(4);
+      const newCats = response.map((res: Cat, index: number) => ({
+        ...res,
+        title: PHRASES[index].title,
+        contents: PHRASES[index].contents,
+      }));
+      setCats(newCats);
+    } catch (e) {
+      throw new Error(`${(e as Error).message}`);
+    }
+  };
+
+  const moveSlide = (e: Event) => {
+    const target = e.currentTarget as HTMLElement;
+    const index = Number(target.id);
+
+    setSlideIndex(index);
+    clearInterval(timer);
+  };
+
+  const moveNext = () => {
+    setSlideIndex((prev: number) => (prev + 1) % cats.length);
+    clearInterval(timer);
+  };
+
+  useEffect(() => {
+    fetchCatImages();
+  }, []);
+
+  useEffect(() => {
+    timer = setInterval(() => {
+      if (cats.length) moveNext();
+    }, 5000);
+  }, [cats, slideIndex]);
+
+  return `
+    <div class=${styles.container}>
+      <header class=${styles.header}>
+        <h2 class=${styles.title}>
+          고양이는<br />
+          어떤 동물일까?
+        </h2>
+      </header>
+      <div class=${styles.slider}>
+        ${Slider({ slideIndex, totalSlide: cats.length, items: cats })}
+        ${ProgressBar({
+          slideIndex,
+          totalSlide: cats.length,
+          onClick: moveSlide,
+        })}
+      </div>
+    </div>
+  `;
+};
+
+export default ServiceSection;
