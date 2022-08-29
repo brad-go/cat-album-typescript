8ad73f134271b7ff90198c2c5425de39b338b1d0
고동현 <dhjk35@naver.com>
2022-08-29 11:36:21 +0900

    Remove(Home): 불필요한 파일 및 import 제거

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,7 @@ import Fact from './fact';
 import Breed from './breed';
 import News from './news';
 import Investment from './investment';
-
-import styles from './Home.module.scss';
+import Footer from './footer';
 
 const Home = () => {
   return `
@@ -17,6 +16,7 @@ const Home = () => {
     ${Section({ children: Breed(), color: 'dark' })}
     ${Section({ children: News(), color: 'dark' })}
     ${Section({ children: Investment(), color: 'light' })};
+    ${Footer()}
   `;
 };
 
