7a5689008ad85d4a17ad2714263643eb6f89ad3d
고동현 <dhjk35@naver.com>
2022-12-19 15:48:08 +0900

    build(cat-album): webpack 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
+  mode: 'development',
+  entry: './src/index.ts',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '/build'),
     // url 변경 시 오류 나는 것을 막아줌
-    publicPath: "/",
+    publicPath: '/',
     // 번들을 추출하기 전에 기존 build 디렉토리를 비워준다.
     // 이전에 CleanWebpackPlugin을 통해서 비웠지만, webpack v5이후로 이렇게 설정 할 수 있다.
     clean: true,
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "build"),
+      directory: path.resolve(__dirname, 'build'),
     },
     // url 변경 시 오류 나는 것을 막아줌
     historyApiFallback: true,
@@ -30,12 +30,12 @@ module.exports = {
         test: /[\.js]$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.ts?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
@@ -43,13 +43,13 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               sourceMap: true,
             },
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sourceMap: true,
             },
@@ -60,9 +60,9 @@ module.exports = {
       {
         test: /\.(jpg|png|svg|gif)$/i,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "public/assets/svgs/[name].[ext]?[hash]",
+            name: 'public/assets/svgs/[name].[ext]?[hash]',
           },
         },
       },
@@ -71,17 +71,18 @@ module.exports = {
   plugins: [
     // 이 옵션 없이 사용하면 빌드 시 개발한 것이 적용되지 않는다. 기존에 만들어둔 파일을 기반으로 html을 만들어준다.
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "style.css",
+      filename: 'style.css',
     }),
     new Dotenv(),
   ],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./src/"),
+      '~': path.resolve(__dirname, './src/'),
     },
-    extensions: [".ts", ".js"],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    extensions: ['.ts', '.js'],
   },
 };
